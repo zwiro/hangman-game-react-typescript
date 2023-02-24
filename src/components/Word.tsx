db53d4db751fd1250c4868e9c9e0b3e2bcca8071
zwiro <josiu100@gmail.com>
2023-02-24 15:35:39 +0100

    Word Component

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react"
+
 function Word() {
-  return <div>word</div>
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([
+    "T",
+    "E",
+    "S",
+    "T",
+  ])
+  return (
+    <div>
+      {guessedLetters.map((letter) => (
+        <span className="word__letter">{letter}</span>
+      ))}
+    </div>
+  )
 }
 
 export default Word
