d018136cfa7a285bbc3c6c000afea14ab43465b3
zwiro <josiu100@gmail.com>
2023-02-24 15:53:48 +0100

    Hangman Component

--- src/components/Hangman.tsx
+++ src/components/Hangman.tsx
@@ -1,7 +1,16 @@
 function Hangman() {
   return (
-    <div>
-      <h1>hgman</h1>
+    <div className="hangman">
+      <div className="hangman__ground" />
+      <div className="hangman__pole" />
+      <div className="hangman__rod" />
+      <div className="hangman__grip" />
+      <div className="hangman__head" />
+      <div className="hangman__body" />
+      <div className="hangman__left-hand" />
+      <div className="hangman__right-hand" />
+      <div className="hangman__left-leg" />
+      <div className="hangman__right-leg" />
     </div>
   )
 }
